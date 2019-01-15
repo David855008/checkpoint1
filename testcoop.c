b1d81fd770a3a5d3b33d644f80318e7c780a92a6
104062108 <davidw70901@gmail.com>
2019-01-15 13:36:06 +0800

    ver1.15.1

---
+++ testcoop.c
@@ -0,0 +1,121 @@
+/* 
+ * file: testcoop.c 
+ */
+#include <8051.h>
+#include "cooperative.h"
+
+/* 
+ * @@@ [2pt] 
+ * declare your global variables here, for the shared buffer 
+ * between the producer and consumer.  
+ * Hint: you may want to manually designate the location for the 
+ * variable.  you can use
+ *        __idata __at (0x30) type var; 
+ * to declare a variable var of the type
+ */
+
+__idata __at(0x32) char buffer;
+__idata __at(0x40) char buffer_temp;
+
+/* [8 pts] for this function
+ * the producer in this test program generates one characters at a
+ * time from 'A' to 'Z' and starts from 'A' again. The shared buffer
+ * must be empty in order for the Producer to write.
+ */
+void Producer(void)
+{
+    /*
+         * @@@ [2 pt]
+         * initialize producer data structure, and then enter
+         * an infinite loop (does not return)
+         */
+    buffer = 0;
+    buffer_temp = 'a';
+    while (1)
+    {
+        /* @@@ [6 pt]
+                 * wait for the buffer to be available, 
+                 * and then write the new data into the buffer */
+        if (buffer != 0)
+            ThreadYield();
+        else
+        {
+            buffer = buffer_temp;
+            if (buffer_temp == 'z')
+                buffer_temp = 'a';
+            else
+                buffer_temp++;
+        }
+    }
+}
+
+/* [10 pts for this function]
+ * the consumer in this test program gets the next item from
+ * the queue and consume it and writes it to the serial port.
+ * The Consumer also does not return.
+ */
+void Consumer(void)
+{
+    /* @@@ [2 pt] initialize Tx for polling */
+    TMOD = 0x20;
+    TH1 = -6;
+    SCON = 0x50;
+    TR1 = 1;
+    while (1)
+    {
+        /* @@@ [2 pt] wait for new data from producer
+                 * @@@ [6 pt] write data to serial port Tx, 
+                 * poll for Tx to finish writing (TI),
+                 * then clear the flag
+                 */
+        while (1)
+        {
+            if (buffer == 0)
+            {
+                ThreadYield();
+            }
+            else
+            {
+                TI = 0;
+                SBUF = buffer;
+                while (!TI)
+                {
+                }
+                buffer = 0;
+            }
+        }
+    }
+}
+
+/* [5 pts for this function]
+ * main() is started by the thread bootstrapper as thread-0.
+ * It can create more thread(s) as needed:
+ * one thread can acts as producer and another as consumer.
+ */
+void main(void)
+{
+    /* 
+           * @@@ [1 pt] initialize globals 
+           * @@@ [4 pt] set up Producer and Consumer.
+           * Because both are infinite loops, there is no loop
+           * in this function and no return.
+           */
+    buffer = 0;
+    buffer_temp = 'a';
+    SBUF = 0;
+    TH1 = -6;
+
+    ThreadCreate(Producer);
+    ThreadCreate(Consumer);
+    ThreadExit();
+}
+
+void _sdcc_gsinit_startup(void)
+{
+    __asm ljmp _Bootstrap
+        __endasm;
+}
+
+void _mcs51_genRAMCLEAR(void) {}
+void _mcs51_genXINIT(void) {}
+void _mcs51_genXRAMCLEAR(void) {}
